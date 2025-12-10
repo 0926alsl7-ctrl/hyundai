c84d2a3598b90bf8a90006788d9c0a33c641c5e5
ASPIRE5-PC\Administrator <0926alsl7@gmail.com>
2025-12-10 22:05:44 +0900

    12/10

---
+++ hyundai.js
@@ -0,0 +1,30 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  duration: 1.2,
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+lenis.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(document.body, {
+  scrollTop(value) {
+    return arguments.length
+      ? lenis.scrollTo(value)
+      : lenis.scroll.instance.scroll;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+});
